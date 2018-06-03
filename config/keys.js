b8d933eb834dd35f3c978f51a012ae308f2c8716
Aakash Thapa <thapa.aakash100@gmail.com>
2018-06-03 17:45:45 +0545

    Google Auth flow finished

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// Logic to say if we are in Production or Development
+if (process.env.NODE_ENV === "production") {
+  // We are in production - return the prod set of keys
+  module.exports = require("./prod");
+} else {
+  // We are in development - return the dev keys!!!
+  module.exports = require("./dev");
+}
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,22 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+// To generate some unique identifier for the user to set in 'cookie'
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize serialized used to turn it back into a User model
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +26,20 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+      // Async operation, so returns a promise
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the gived profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
